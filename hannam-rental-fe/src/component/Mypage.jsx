0b219dd5c86726be64bd74782babd74d4cb49349
seoyoomi <seoyoomi1102@gmail.com>
2025-03-02 20:24:14 +0900

    [Feat]: Mypage 상품 대여일, 반납일 계산 기능 구현

--- hannam-rental-fe/src/component/Mypage.jsx
+++ hannam-rental-fe/src/component/Mypage.jsx
@@ -4,6 +4,13 @@ import user from "../assets/user.jpg"
 import umbrella from "../assets/umbrella.jpg"
 
 const Mypage  = () => {
+    const today = new Date();
+    const rentalDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일`;
+    
+    const returnDay = new Date(today);
+    returnDay.setDate(today.getDate() + 4);
+    const returnDate = `${returnDay.getFullYear()}년 ${returnDay.getMonth() + 1}월 ${returnDay.getDate()}일`;
+
     return(
         <div className="mypage">
             <Header/>
@@ -16,9 +23,10 @@ const Mypage  = () => {
                 <div className='rentalList'>
                     <img className ="rentalImage" src={umbrella}/>
                     <div className='rentalInfo'>
-                    <p>제품명: </p>
-                    <p>대여일: </p>
-                    <p>반납일: </p>
+                    <p>제품명: 우산 </p>
+                    <p>대여일: {rentalDate} </p>
+                    <p>반납일: {returnDate} </p>
+                    <p>대여학과: </p>
                     </div>
                 </div>
             </div>
