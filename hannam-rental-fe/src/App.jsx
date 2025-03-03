bd87c05392b0581c703d67e8754a17359521e460
seoyoomi <seoyoomi1102@gmail.com>
2025-03-03 19:43:48 +0900

    [Test]: 대여 정보를 저장하는 스테이트 생성

--- hannam-rental-fe/src/App.jsx
+++ hannam-rental-fe/src/App.jsx
@@ -8,8 +8,36 @@ import RentalPage from './component/RentalPage'
 import CollegeSelect from './component/CollegeSelect'
 import Sidebar from './component/Sidebar'
 import RentalApplication from './component/RentalApplication'
+import { useReducer } from 'react'
+
+const mockData = [
+  {
+    RentalId: 1,
+    createdDate : new Date("2025-03-01").getTime(),
+    productId: 1,
+    rentalSpace: "스마트융합대학",
+  },
+  {
+    RentalId: 2,
+    createdDate : new Date("2025-03-02").getTime(),
+    productId: 1,
+    rentalSpace: "공과대학",
+  },
+  {
+    RentalId: 3,
+    createdDate : new Date("2025-03-03").getTime(),
+    productId: 2,
+    rentalSpace: "경상대학",
+  },
+]
+
+function reducer(state, action){
+  return state;
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
   return (
     <div className='App'>
       <Routes>
